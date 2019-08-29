14f95882856044be8e3f3c9be0ec11b8626f051a
puiutucutu <>
2019-08-29 15:31:17 -0400

    Fix ramp break method

--- src/utils/getColor.js
+++ src/utils/getColor.js
@@ -1,9 +1,9 @@
-import chroma from "chroma-js"
+import chroma from "chroma-js";
 
 // getColor :: [Number] -> Number -> String
 const getColor = rampValues => {
   const colors = chroma.brewer.Blues;
-  const rampDomain = chroma.limits(rampValues, "q");
+  const rampDomain = chroma.limits(rampValues, "e");
   const colourScale = chroma.scale(colors).domain(rampDomain);
 
   return value => colourScale(value).alpha(0.8).rgba();
